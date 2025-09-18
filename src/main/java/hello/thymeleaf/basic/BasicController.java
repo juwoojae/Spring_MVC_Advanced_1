4c73263a4e76a6c2b85ceab9d5a6519f6a44378d
Juwoojae <juwqq1234@naver.com>
2025-09-18 18:46:04 +0900

    변수 - SpringEL

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,9 +1,15 @@
 package hello.thymeleaf.basic;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Controller 클래스 안의 메서드에서 return 이 String 이라면
@@ -25,5 +31,36 @@ public class BasicController {
     public String textUnescaped(Model model){
         model.addAttribute("data", "<b>Hello Spring!</b>");
         return "basic/text-unescaped";
+    }
+    @GetMapping("/variable")
+    public String variable(Model model){
+        User userA = new User("userA", 10); //View 로 Model 넘겨준후, Model은 이것으로 화면 렌더링
+        User userB = new User("userB", 20);
+
+        List<User> list=new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);  //Model 에 key - value 의 형태로 들어간다
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+
+    @Data
+    static class User{
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 }
