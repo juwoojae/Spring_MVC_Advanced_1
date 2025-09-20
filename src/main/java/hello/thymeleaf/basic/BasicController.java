1eb6e964879d946b81f1ba52893cff8ad34b800e
Juwoojae <juwqq1234@naver.com>
2025-09-20 19:41:01 +0900

    블록

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -124,6 +124,11 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "basic/comments";
     }
+    @GetMapping("/block")
+    public String block(Model model){
+        addUsers(model);
+        return "basic/block";
+    }
 
     private void addUsers(Model model){
         List<User> list = new ArrayList<>();
